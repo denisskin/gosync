ee74de731aecdb378894d5d5875d349780830b6e
denisskin <denis@begom.com>
2017-03-06 02:24:13 +0300

    add sync Event

--- event.go
+++ event.go
@@ -1,1 +1,21 @@
 package gosync
+
+// Event is asynchronous event.
+// Event is a little faster than sync.WaitGroup
+type Event chan struct{}
+
+// NewEvent makes asynchronous event
+func NewEvent() Event {
+	return make(Event)
+}
+
+// Done triggers the event.
+// It can be called only once.
+func (ev Event) Done() {
+	close(ev)
+}
+
+// Wait waits when the event happens.
+func (ev Event) Wait() {
+	<-ev
+}
