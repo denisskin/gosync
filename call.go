0d83b93d877b052f0d6912bbcf6e5a2fbd14c3dc
denisskin <denis@begom.com>
2017-08-04 11:54:20 +0300

    add async Call; CallGroup

---
+++ call.go
@@ -0,0 +1,37 @@
+package gosync
+
+import "sync"
+
+type CallGroup struct {
+	wg  sync.WaitGroup
+	mx  sync.Mutex
+	err error
+}
+
+func (c *CallGroup) Add(fn func() error) *CallGroup {
+	c.wg.Add(1)
+	go func() {
+		defer c.wg.Done()
+		if err := fn(); err != nil {
+			c.mx.Lock()
+			defer c.mx.Unlock()
+			if c.err == nil {
+				c.err = err
+			}
+		}
+	}()
+	return c
+}
+
+func (c *CallGroup) Wait() error {
+	c.wg.Wait()
+	return c.err
+}
+
+func Call(fn ...func() error) error {
+	var c CallGroup
+	for _, f := range fn {
+		c.Add(f)
+	}
+	return c.Wait()
+}
