1c6d9aa51bd106063ca313e1ff512ba454e880f5
denis <denis@begom.com>
2019-01-25 23:16:01 +0300

    add map version

--- map.go
+++ map.go
@@ -12,7 +12,14 @@ import (
 // A Map is safe for use by multiple goroutines simultaneously.
 type Map struct {
 	mx   sync.RWMutex
+	ver  uint64
 	vals map[interface{}]interface{}
+}
+
+func NewMap(values map[interface{}]interface{}) Map {
+	return Map{
+		vals: values,
+	}
 }
 
 func normKey(key interface{}) interface{} {
@@ -26,36 +33,62 @@ func (m *Map) Clear() {
 	m.mx.Lock()
 	defer m.mx.Unlock()
 	m.vals = map[interface{}]interface{}{}
+	m.ver++
 }
 
 func (m *Map) Set(key, value interface{}) {
+	key = normKey(key)
+
 	m.mx.Lock()
 	defer m.mx.Unlock()
 	if m.vals == nil {
 		m.vals = map[interface{}]interface{}{}
 	}
-	m.vals[normKey(key)] = value
+	m.vals[key] = value
+	m.ver++
+}
+
+func (m *Map) Increment(key interface{}, val int64) {
+	key = normKey(key)
+
+	m.mx.Lock()
+	defer m.mx.Unlock()
+	if m.vals == nil {
+		m.vals = map[interface{}]interface{}{}
+	}
+	if v, ok := m.vals[key].(int64); ok {
+		val += v
+	}
+	m.vals[key] = val
+	m.ver++
 }
 
 func (m *Map) Delete(key interface{}) {
+	key = normKey(key)
+
 	m.mx.Lock()
 	defer m.mx.Unlock()
-	delete(m.vals, normKey(key))
+	delete(m.vals, key)
+	m.ver++
 }
 
 func (m *Map) Get(key interface{}) interface{} {
+	key = normKey(key)
+
 	m.mx.RLock()
 	defer m.mx.RUnlock()
-	if v, ok := m.vals[normKey(key)]; ok {
+	if v, ok := m.vals[key]; ok {
 		return v
 	}
 	return nil
 }
 
 func (m *Map) Exists(key interface{}) bool {
+	key = normKey(key)
+
 	m.mx.RLock()
 	defer m.mx.RUnlock()
-	_, ok := m.vals[normKey(key)]
+	_, ok := m.vals[key]
 	return ok
 }
 
@@ -63,6 +96,12 @@ func (m *Map) Size() int {
 	m.mx.RLock()
 	defer m.mx.RUnlock()
 	return len(m.vals)
+}
+
+func (m *Map) Version() uint64 {
+	m.mx.RLock()
+	defer m.mx.RUnlock()
+	return m.ver
 }
 
 func (m *Map) KeyValues() map[interface{}]interface{} {
@@ -74,6 +113,17 @@ func (m *Map) KeyValues() map[interface{}]interface{} {
 		res[k] = v
 	}
 	return res
+}
+
+func (m *Map) Keys() []interface{} {
+	m.mx.RLock()
+	defer m.mx.RUnlock()
+
+	vv := make([]interface{}, 0, len(m.vals))
+	for key := range m.vals {
+		vv = append(vv, key)
+	}
+	return vv
 }
 
 func (m *Map) Values() []interface{} {
@@ -111,6 +161,7 @@ func (m *Map) Pop() (key, value interface{}) {
 	defer m.mx.Unlock()
 	for key, value = range m.vals {
 		delete(m.vals, key)
+		m.ver++
 		return
 	}
 	return
@@ -121,12 +172,18 @@ func (m *Map) PopAll() (values map[interface{}]interface{}) {
 	defer m.mx.Unlock()
 	values = m.vals
 	m.vals = nil
+	m.ver++
 	return
 }
 
 func (m *Map) RandomValue() interface{} {
 	_, v := m.Random()
 	return v
+}
+
+func (m *Map) RandomKey() interface{} {
+	k, _ := m.Random()
+	return k
 }
 
 func (m *Map) Random() (key, value interface{}) {
